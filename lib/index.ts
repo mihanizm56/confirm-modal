e6d44b3ffceb3e9feacc4febadc1d9e95cbcc5d4
mihanizm56 <m727507.56@mail.ru>
2020-08-12 22:48:25 +0300

    build(configure-package): update

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,7 @@ export {
   setConfirmModalAction,
   closeConfirmModalAction,
 } from './redux-module/actions';
-
+export { confirmModalModuleReducer } from './redux-module/reducer';
 export { ConfirmModal } from './components';
 export { CONFIRM_MODALS_REDUCER_NAME } from './redux-module/constants';
 export { ConfirmModalStateType } from './types/types';
