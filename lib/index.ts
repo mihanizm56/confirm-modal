e7fd26d2188c9da83b689cf43f4c2abce13474d8
mihanizm56 <m727507.56@mail.ru>
2020-09-29 16:22:59 +0300

    build(configure-package): fix exports

--- lib/index.ts
+++ lib/index.ts
@@ -5,6 +5,7 @@ export {
   CONFIRM_MODAL_SAGA_NAME,
   CONFIRM_MODALS_REDUCER_NAME,
   confirmModalModuleReducer,
+  confirmModalLoadingStopAction,
 } from './redux-module';
 export { ConfirmModal } from './components';
 export { ConfirmModalStateType } from './types';
